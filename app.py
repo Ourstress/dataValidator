f1cd5b8f59f061027d0d72b8c214d1403c3af9ad
SG <sgng1@e.ntu.edu.sg>
2019-09-04 03:31:08 +0100

    added goodtables library and fetched some csv data using requests

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from goodtables import validate
+import csv
+import requests
+
+CSV_URL = "http://www.stat.umn.edu/geyer/3701/data/p3p3.csv"
+
+with requests.get(CSV_URL, stream=True) as r:
+	lines = (line.decode('utf-8') for line in r.iter_lines())
+	csvData = ""
+	for row in csv.reader(lines):
+		csvData = csvData + str(row)
+
+report = validate(lines)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "<h1>hello world</h1><p>Hello hello there</p>"
+	return f"<h1>hello world</h1><p>report has table count of {report['table-count'] }</p><p>{csvData}</p>"
 
 @app.route("/hello.html")
 def hey():
