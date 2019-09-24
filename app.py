64f90f519c9a583bd85ecefef8bf4a3f4ec1fee0
SG <sgng1@e.ntu.edu.sg>
2019-09-24 08:40:41 +0100

    using pyPDF2 to read pdfs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import requests
 import pandas as pd 
+import PyPDF2
+import io
 
 app = Flask(__name__)
 
@@ -11,8 +13,9 @@ def hello():
 @app.route('/csv', methods=['GET']) 
 def csv():
     return '''
+        <p>Validates CSV and PDF files</p>
         <form action="/csv" method="post" enctype="multipart/form-data">
-            <input name="data" type="file" accept=".csv" />
+            <input name="data" type="file" accept=".csv,.pdf" />
             <input value="check csv file" type="submit" />
         </form>
     '''
@@ -21,11 +24,16 @@ def csv():
 def do_csv():
     if request.files:
         dataFile = request.files['data']
-        data = pd.read_csv(dataFile, index_col=0, header=None)
-        return f"""
-        <p>File name: {dataFile.filename}</p>
-        {data.head().to_html()}
-        """
+        # Reading PDF https://stackoverflow.com/questions/55481813/how-to-read-pdf-from-in-memory-uploaded-file-python-3-6
+        if dataFile.filename.endswith('.pdf'):
+            data = PyPDF2.PdfFileReader(io.BytesIO(dataFile.read())).getPage(0)
+        if dataFile.filename.endswith('.csv'):
+            data = pd.read_csv(dataFile, index_col=0, header=None)
+        return str(data)
+        # return f"""
+        # <p>File name: {dataFile.filename}</p>
+        # {data.head().to_html()}
+        # """
     else:
         return "<p>Login failed.</p>"
 
